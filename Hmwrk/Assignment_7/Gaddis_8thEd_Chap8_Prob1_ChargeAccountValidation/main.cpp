09e9b1800d4b421b517adee3b91bfc0b6e84313b
jemerson93 <jason.emerson93@gmail.com>
2016-11-29 12:38:17 -0800

    Signed-off-by: jemerson93 <jason.emerson93@gmail.com>

---
+++ Hmwrk/Assignment_7/Gaddis_8thEd_Chap8_Prob1_ChargeAccountValidation/main.cpp
@@ -0,0 +1,37 @@
+/* 
+ * File:   main.cpp
+ * Author: Jason Emerson
+ * Created on November 29th, 2016, 12:10 PM
+ * Purpose: User inputs a charge number and checks it to see if it is valid with the current charge numbers in an array
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    const int SIZE = 18;
+    int num[SIZE] = {5658845,4520125,7895122,8777541,8451277,1302850,8080152,
+                     4562555,5552012,5050552,7825877,1250255,1005231,6545231,
+                     3852085,7576651,7881200,4581002};
+    
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    cout<<"Hello World"<<endl;
+
+    //Exit
+    return 0;
+}
+
